01626ac6baeafeca8e6c12fc14dd50e8b860ce37
Jeff Stock <jeffcstock@gmail.com>
2017-01-15 20:40:08 -0800

    Add reducers and actions

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 24 },
+    { title: 'The Dark Tower', pages: 400 },
+    { title: '1984', pages: 103 },
+  ]
+}
